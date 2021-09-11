42ce638082e83385441b17fb2bda85f5b010bf61
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-11 15:29:10 +0530

    Solved: Added Part Time Employee & Wages.

--- src/com/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/employeewagecomputation/EmployeeWageComputation.java
@@ -6,16 +6,19 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_WAGE_PER_HOUR = 20;
 
 		int empHrs = 0;
 		int empWage = 0;
 
 		Random num = new Random();
-		int empCheck = num.nextInt(2);
+		int empCheck = num.nextInt(3);
 
 		if (empCheck == IS_FULL_TIME) {
 			empHrs = 8;
+		} else if (empCheck == IS_PART_TIME) {
+			empHrs = 4;
 		} else {
 			empHrs = 0;
 		}
