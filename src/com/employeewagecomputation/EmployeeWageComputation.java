3b926bb2ae54c36da1b57bfe117049d705a685ac
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-11 15:33:45 +0530

    Solved: Modified Code Using Switch Statement.

--- src/com/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/employeewagecomputation/EmployeeWageComputation.java
@@ -3,23 +3,25 @@ package com.employeewagecomputation;
 import java.util.Random;
 
 public class EmployeeWageComputation {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_WAGE_PER_HOUR = 20;
 
 	public static void main(String[] args) {
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_WAGE_PER_HOUR = 20;
-
 		int empHrs = 0;
 		int empWage = 0;
 
 		Random num = new Random();
 		int empCheck = num.nextInt(3);
 
-		if (empCheck == IS_FULL_TIME) {
+		switch (empCheck) {
+		case IS_FULL_TIME:
 			empHrs = 8;
-		} else if (empCheck == IS_PART_TIME) {
+			break;
+		case IS_PART_TIME:
 			empHrs = 4;
-		} else {
+			break;
+		default:
 			empHrs = 0;
 		}
 
