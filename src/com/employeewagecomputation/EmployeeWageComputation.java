0456b91183405749f9a2d450c2cb778655fd63e0
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-11 15:21:57 +0530

    Welcome to Employee Wage Computation Program.

--- src/com/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/employeewagecomputation/EmployeeWageComputation.java
@@ -3,8 +3,7 @@ package com.employeewagecomputation;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to Employee Wage Computation Program");
 	}
 
 }
