9a88bb2569109dc99da97860e8e8547824cdd9ef
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-11 15:23:44 +0530

    Solved: Employee Is Present Or Absent Problem.

--- src/com/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/employeewagecomputation/EmployeeWageComputation.java
@@ -1,9 +1,20 @@
 package com.employeewagecomputation;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+		int IS_FULL_TIME = 1;
+
+		Random num = new Random();
+		int empCheck = num.nextInt(2);
+
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
